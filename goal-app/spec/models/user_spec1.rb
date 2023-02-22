36e784e566de3dd8a5242697f6eb7b128c976a8c
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-22 16:27:56 -0500

    Make changes to project

--- goal-app/spec/models/user_spec1.rb
+++ goal-app/spec/models/user_spec1.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "User", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+RSpec.describe "User", type: :models do
+  # describe "GET /index" do
+  #   pending "add some examples (or delete) #{__FILE__}"
+  # end
 
   it { should validate_presence_of(:username) }
   it { should validate_length_of(:password).is_at_least(6) }
